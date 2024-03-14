8a9c100b7aa64e09754450cd5a9d40bfcd9c5e1d
Ming Du <mingdu@mings-mbp.lan>
2024-03-13 23:11:02 -0500

    Multipage

--- tab_extractor/loader.py
+++ tab_extractor/loader.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import tqdm
 
+import tab_extractor.util as util
 
 class Loader:
 
@@ -14,10 +15,19 @@ class Loader:
 
 
 class VideoLoader(Loader):
-    def __init__(self, path):
+    def __init__(self, path, bounding_box=None):
+        """
+        The constructor.
+
+        :param path: str. Path to the video.
+        :param bounding_box: tuple[int]. The bounding box of the region in a video frame that contains the
+                             actual tab or score given as (y_start, x_start, y_end, x_end).
+                             If None, the entire frame will be used.
+        """
         super().__init__(path)
         self.fps = 0
         self.vidcap = None
+        self.bounding_box = bounding_box
         self.images = []
 
     def get_start_end_step_in_frames(self, start_s=None, end_s=None, step_s=1):
@@ -40,6 +50,8 @@ class VideoLoader(Loader):
         while i_frame < end_f:
             _, image = self.vidcap.read()
             if i_frame > start_f and (i_frame - start_f) % step_f == 0:
+                if self.bounding_box is not None:
+                    image = util.apply_bounding_box(image, self.bounding_box)
                 self.images.append(image)
             i_frame += 1
             pbar.update(1)
--- tab_extractor/processor.py
+++ tab_extractor/processor.py
@@ -1,18 +1,26 @@
 import logging
 
 import numpy as np
-import matplotlib.pyplot as plt
+from PIL import Image
 import cv2
 
 from tab_extractor.loader import Loader
 
 
 class Processor:
-    def __init__(self, loader: Loader):
+    def __init__(self, loader: Loader, page_aspect_ratio=(11, 8.5)):
+        """
+        The constructor.
+
+        :param loader: Loader.
+        :param page_aspect_ratio: tuple[float]. The desired page aspect ratio in (height, width). This will determine
+                                  the number of lines to place in each page.
+        """
         self.loader = loader
         self.unique_images = []
         self.diff_threshold = 10
-        self.full_image = None
+        self.pages = []
+        self.page_aspect_ratio = page_aspect_ratio
 
     def build_unique_images(self):
         self.unique_images = [self.loader.images[0]]
@@ -22,12 +30,38 @@ class Processor:
             if diff > self.diff_threshold:
                 self.unique_images.append(img)
 
+    def find_num_lines_per_page(self):
+        image_shape = self.loader.images[0].shape
+        n = int(np.floor(image_shape[1] / self.page_aspect_ratio[1] * self.page_aspect_ratio[0] / image_shape[0]))
+        return n
+
     def stitch_unique_images(self):
-        self.full_image = np.concatenate(self.unique_images, axis=0)
+        lines_per_page = self.find_num_lines_per_page()
+        n_pages = int(np.ceil(len(self.unique_images) / lines_per_page))
+        i_page = 0
+        while i_page < n_pages:
+            ind_st = i_page * lines_per_page
+            ind_end = min((i_page + 1) * lines_per_page, len(self.unique_images))
+            page = np.concatenate(self.unique_images[ind_st:ind_end], axis=0)
+            self.pages.append(page)
+            i_page += 1
+        max_shape = [np.max([p.shape[0] for p in self.pages]), np.max([p.shape[1] for p in self.pages])]
+        if len(self.pages[0].shape) == 3:
+            max_shape.append(self.pages[0].shape[-1])
+        for i in range(n_pages):
+            page = np.full(max_shape, fill_value=255)
+            img = self.pages[i]
+            ys = 0
+            xs = max_shape[1] // 2 - img.shape[1] // 2
+            page[ys:ys + img.shape[0], xs:xs + img.shape[1]] = img
+            self.pages[i] = page
 
     def run(self):
         self.build_unique_images()
         self.stitch_unique_images()
 
-    def save_stitched(self, path):
-        cv2.imwrite(path, self.full_image)
+    def save_pages(self, path):
+        image_list = [Image.fromarray(p.astype('uint8')) for p in self.pages]
+        image_list[0].save(
+            path, "PDF", resolution=100.0, save_all=True, append_images=image_list[1:]
+        )
